ecd8125370d3f9fd105ee7491394866bb2a2f534
tharu <tharunreddy057@gmail.com>
2021-09-14 13:04:01 +0530

    second

--- src/gitdemoproject/demo/demoproject.java
+++ src/gitdemoproject/demo/demoproject.java
@@ -1,5 +1,20 @@
 package gitdemoproject.demo;
 
 public class demoproject {
+	
 
-}
+		public static void main(String args[]) {
+			int a[]= {10,20,30,40};
+			//for (int i=0;i<a.length;i++)
+				//try {
+				System.out.println(a[5]);
+				
+				
+		//}
+			//catch(Exception e) {
+			//System.out.println(e);
+			}
+	}
+	//}
+
+
